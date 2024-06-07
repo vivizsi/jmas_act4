77502ce700ef4dba354eee779bc1952af4ff07cb
vivizsi <144732898+vivizsi@users.noreply.github.com>
2024-06-07 14:04:34 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,241 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cardenas/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MiAplicacion());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiAplicacion extends StatelessWidget {
+  const MiAplicacion({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'JMAS',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PaginaSesion(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+// ignore: must_be_immutable
+class PaginaSesion extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  // ignore: prefer_final_fields
+  CollectionReference _usersCollection =
+      FirebaseFirestore.instance.collection('usuario');
+
+  PaginaSesion({super.key});
+
+  void _login(BuildContext context) async {
+    String email = _emailController.text;
+    String contrasena = _passwordController.text;
+
+    QuerySnapshot querySnapshot = await _usersCollection
+        .where('email', isEqualTo: email)
+        .where('password', isEqualTo: contrasena)
+        .get();
+
+    if (querySnapshot.size > 0) {
+      // Inicio de sesión exitoso, redirigir a la página principal
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => PaginaInicio(email)),
+      );
+    } else {
+      // Mostrar un diálogo de error en caso de inicio de sesión fallido
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error de inicio de sesión'),
+            content:
+                const Text('Credenciales incorrectas. Inténtalo de nuevo.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Aceptar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void _registro(BuildContext context) async {
+    String email = _emailController.text;
+    String contrasena = _passwordController.text;
+
+    QuerySnapshot querySnapshot =
+        await _usersCollection.where('email', isEqualTo: email).get();
+
+    if (querySnapshot.size > 0) {
+      // Mostrar un diálogo de error en caso de que el usuario ya esté registrado
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error de registro'),
+            content: const Text('El usuario ya está registrado.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Aceptar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // Registrar al usuario en Firestore
+      await _usersCollection.add({
+        'email': email,
+        'password': contrasena,
+      });
+
+      // Mostrar un diálogo de registro exitoso
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Registro exitoso'),
+            content: const Text('El usuario ha sido registrado correctamente.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Aceptar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        centerTitle: true,
+        title: const Text('JMAS'),
+        backgroundColor: const Color(0xff609ad0),
+        elevation: 8,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            const Text(
+              "Inicio de sesión o registro",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                height: 2.5,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Correo electrónico',
+              ),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Contraseña',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Iniciar sesión',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(340, 40),
+                backgroundColor: const Color(0xff85afed),
+              ),
+              onPressed: () => _login(context),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Registrarse',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(340, 40),
+                backgroundColor: const Color(0xff85afed),
+              ),
+              onPressed: () => _registro(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaginaInicio extends StatelessWidget {
+  final String _email;
+
+  const PaginaInicio(this._email, {super.key});
+
+  void _cerrarSesion(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => PaginaSesion()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Jmas'),
+        backgroundColor: const Color(0xff609ad0),
+        elevation: 8,
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+                image: NetworkImage(
+                    "https://raw.githubusercontent.com/vivizsi/img-IOS_Flutterflow/main/f4.jpg")),
+            const Text('¡Inicio de sesión exitoso!'),
+            const SizedBox(height: 20.0),
+            Text('Correo electrónico: $_email'),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text('Cerrar sesión'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(340, 40),
+                backgroundColor: const Color(0xff609ad0),
+              ),
+              onPressed: () => _cerrarSesion(context),
+            ),
+          ],
         ),
       ),
     );
